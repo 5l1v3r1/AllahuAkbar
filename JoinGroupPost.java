aa6f0f9ad5975e520f987fc3384c7de02aef5e78
Yggdrassil <Yggdrassil>
2016-05-16 11:54:24 +0200

    joingrouppost creado y funcionando

---
+++ JoinGroupPost.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class JoinGroupPost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class JoinGroupPost extends Post
+{
+    // instance variables - replace the example below with your own
+    private String nombreGrupo;
+    
+
+    /**
+     * Constructor for objects of class JoinGroupPost
+     */
+    public JoinGroupPost(String author, String nombreGrupo)
+    {
+        // initialise instance variables
+        super(author);
+        this.nombreGrupo = nombreGrupo; 
+        
+    }
+
+   
+    }
